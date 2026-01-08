d785998ce7b98c635f654e73cc79814b4334b38a
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-01-08 13:04:14 +0800

    Another verifyTls adjustment

--- santricity/santricity.psm1
+++ santricity/santricity.psm1
@@ -444,7 +444,26 @@ function Invoke-SANtricityRequest {
             $handler = [System.Net.Http.HttpClientHandler]::new()
             if (-not $cfg.VerifySsl) {
                 Write-Verbose "Setting ServerCertificateCustomValidationCallback to skip TLS verification"
-                $handler.ServerCertificateCustomValidationCallback = { $true }
+                try {
+                    $handler.ServerCertificateCustomValidationCallback = { $true }
+                } catch {
+                    Write-Verbose "Handler callback not supported; using ServicePointManager fallback"
+                }
+                # Fallback for older .NET where HttpClientHandler callback doesn't work
+                if (-not ([System.Management.Automation.PSTypeName]'TrustAllCertsPolicy').Type) {
+                    Add-Type -TypeDefinition @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+    public bool CheckValidationResult(ServicePoint svcPoint, X509Certificate cert, WebRequest req, int problem) {
+        return true;
+    }
+}
+"@
+                }
+                [System.Net.ServicePointManager]::CertificatePolicy = [TrustAllCertsPolicy]::new()
+                [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
+                Write-Verbose "Applied ServicePointManager TLS bypass for compatibility"
             } else {
                 Write-Verbose "TLS certificate validation is enabled"
             }
